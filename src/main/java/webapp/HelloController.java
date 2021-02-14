fcf9b80b9398cbbf01406f7c197b7d51f18757cc
Raul Castro <raulc27@gmail.com>
2021-02-14 12:57:31 -0300

    adicionando controller... para /

---
+++ src/main/java/webapp/HelloController.java
@@ -0,0 +1,14 @@
+package webapp;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Ola do Spring Boot!";
+	}
+}
